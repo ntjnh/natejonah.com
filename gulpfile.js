f1bfd5070654c37cfa907cbb3b2082be631ec4d8
Nate Jonah <nate@natejonah.com>
2019-11-11 22:59:34 +0000

    Create browser-sync serve task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { dest, series, src, watch } = require("gulp");
 const minifyCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
 
 const sass = require("gulp-sass");
       sass.compiler = require("node-sass");
@@ -8,7 +9,19 @@ const styles = () => {
     return src("scss/**/*.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(minifyCSS())
-        .pipe(dest("css"));
+        .pipe(dest("css"))
+        .pipe(browserSync.stream());
+};
+
+const serve = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    watch("scss/**/*.scss", styles);
+    watch("./index.html").on("change", browserSync.reload);
 };
 
 const watchStyles = () => watch("scss/**/*.scss", styles);
@@ -16,5 +29,6 @@ const watchStyles = () => watch("scss/**/*.scss", styles);
 module.exports = {
     default: series(styles, watchStyles),
     sass: styles,
-    watch: watchStyles
+    watch: watchStyles,
+    serve: serve
 };
