bd5a6198084a4a7aeb92473af2279a2a93e9ef21
Nate Jonah <nate@natejonah.com>
2019-12-29 13:52:34 +0000

    Remove code within main function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 function main() {
-  $(".nav__link").click(function(e) {
-    e.preventDefault();
-
-    $("html, body").animate({
-      scrollTop: $($.attr(this, "href")).offset().top
-    }, 1000);
-  });
+    
 }
 
 $(document).ready(main);
