8ce230000737353a5eceb2919c3db87396a79c2c
Nate Jonah <nate@natejonah.com>
2019-12-29 15:46:34 +0000

    Add browser-sync task to gulpfile and amend styles task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,25 @@ const { dest, series, src, watch } = require("gulp");
 const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
 const cssmin = require("gulp-clean-css");
+const browserSync = require("browser-sync");
 
 const styles = () => {
     return src("scss/style.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(cssmin())
-        .pipe(dest("css"));
+        .pipe(dest("css"))
+        .pipe(browserSync.stream());
+};
+
+const serve = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    watch("scss/**/*.scss", styles);
+    watch("index.html").on("change", browserSync.reload);
 };
 
 const watchSass = () => watch("./scss/**/*.scss", styles);
@@ -15,5 +28,6 @@ const watchSass = () => watch("./scss/**/*.scss", styles);
 module.exports = {
     default: series(styles, watchSass),
     sass: styles,
-    watch: watchSass
+    watch: watchSass,
+    serve: serve
 };
